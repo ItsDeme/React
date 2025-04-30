cef67ed4ad273fb3a3545df0448eb8f2b78cfd12
svimonishvilidemna <111353160+svimonishvilidemna@users.noreply.github.com>
2025-04-30 20:01:00 +0400

    initialization

---
+++ React_1/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+     <div className='card'>
+      <img src="https://cdna.artstation.com/p/assets/images/images/059/500/400/large/anasxart-untitled34-20220723170548.jpg?1676516078" alt="" />
+      <p>Uchiha Sasuke</p>
+     </div>
+     <div className='card'>
+      <img src="https://cdna.artstation.com/p/assets/images/images/059/500/400/large/anasxart-untitled34-20220723170548.jpg?1676516078" alt="" />
+      <p>Uchiha Sasuke</p>
+     </div>
+     <div className='card'>
+      <img src="https://cdna.artstation.com/p/assets/images/images/059/500/400/large/anasxart-untitled34-20220723170548.jpg?1676516078" alt="" />
+      <p>Uchiha Sasuke</p>
+     </div>
+     </div>
+     </>
+  )
+}
+
+export default App
